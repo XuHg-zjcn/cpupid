94f73e18bbb7af7ece3d7317cfe77ff2252542fa
徐瑞骏 <1687701765@qq.com>
2022-02-20 11:15:53 +0800

    PID param move out code

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ N = 4
 m = 800
 M = 2800
 
+Target = 72
+Kp = 100
+Ki = 20
+Kd = 40
+
 def get_temp():
     temps = psutil.sensors_temperatures()
     return temps['acpitz'][0].current
@@ -21,18 +26,21 @@ def get_clock():
     return int(psutil.cpu_freq().current)
 
 if __name__ == '__main__':
-    Target = 72
     delta = Target - get_temp()
-    inte = (get_clock() - delta*100)/2
+    inte = (get_power() - delta*Kp)/Ki
     d_ = delta
     while True:
         now = get_temp()
         delta = Target - now
         diff = delta - d_
         d_ = delta
-        if abs(diff)<=1 or abs(delta)<=1:
+        if abs(diff) <= 1 or abs(delta) <= 2:
             diff = 0
-        MHz = diff*50 + delta*100 + inte*2
+        if abs(diff) >= 4:
+            diff *= 1.5
+        if abs(delta) <= 2:
+            delta /= 2
+        MHz = diff*Kd + delta*Kp + inte*Ki
         if m <= MHz <= M:
             inte += delta
         else:
@@ -40,6 +48,6 @@ if __name__ == '__main__':
                 MHz = m
             if MHz > M:
                 MHz = M
-        print(delta, inte, MHz)
+        print(f'{-delta:+2.0f}, {MHz:4.0f}')
         set_clock(MHz)
         time.sleep(1)
