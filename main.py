464abed9749bb7a767f185131698613f4105aa0a
徐瑞骏 <1687701765@qq.com>
2022-02-20 11:40:00 +0800

    use `Estimate Power(W)` instead `Frequency(MHz)` as control ouput

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import time
 import psutil
+import math
 
 N = 4
 m = 800
 M = 2800
 
 Target = 72
-Kp = 100
-Ki = 20
-Kd = 40
+Kp = 2
+Ki = 0.75
+Kd = 0.5
 
 def get_temp():
     temps = psutil.sensors_temperatures()
@@ -23,7 +24,34 @@ def set_clock(MHz):
             f.write(str(kHz))
 
 def get_clock():
-    return int(psutil.cpu_freq().current)
+    f = int(psutil.cpu_freq().current)
+    if f > M:
+        return M - 100
+    if f < m:
+        return m + 100
+    return f
+
+# P = f*(1 + f)*5
+# f = (sqrt(1 + 4*P/5) - 1)/2
+def set_power(x):
+    if x < 0:
+        x = 0
+    clock = (math.sqrt(1 + 4*x/5) - 1)/2
+    clock *= 1000
+    ret = 0
+    if x < 0 or clock < m:
+        clock = m
+        ret = -1
+    elif clock > M:
+        clock = M
+        ret = 1
+    set_clock(clock)
+    return ret, clock
+
+def get_power():
+    clock = get_clock()/1000
+    return clock*(1 + clock)*5
+
 
 if __name__ == '__main__':
     delta = Target - get_temp()
@@ -36,18 +64,11 @@ if __name__ == '__main__':
         d_ = delta
         if abs(diff) <= 1 or abs(delta) <= 2:
             diff = 0
-        if abs(diff) >= 4:
-            diff *= 1.5
-        if abs(delta) <= 2:
-            delta /= 2
-        MHz = diff*Kd + delta*Kp + inte*Ki
-        if m <= MHz <= M:
+        if abs(delta) <= 1:
+            delta *= 0.75
+        power = diff*Kd + delta*Kp + inte*Ki
+        r, c = set_power(power)
+        if r == 0 or r*delta<0:  # r == 0 或 r与delta异号
             inte += delta
-        else:
-            if MHz < m:
-                MHz = m
-            if MHz > M:
-                MHz = M
-        print(f'{-delta:+2.0f}, {MHz:4.0f}')
-        set_clock(MHz)
+        print(f'{-delta:+2.0f}, {power:4.1f}, {c:4.0f}')
         time.sleep(1)
